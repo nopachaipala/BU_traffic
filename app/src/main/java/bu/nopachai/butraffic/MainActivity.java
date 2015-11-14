e781b2cde48d5ad6cfcd5ac9e57ee3ca419fb431
BookBik <nopachai.pala@bumail.net>
2015-11-14 15:00:39 +0700

    Sound Effect Successful

--- app/src/main/java/bu/nopachai/butraffic/MainActivity.java
+++ app/src/main/java/bu/nopachai/butraffic/MainActivity.java
@@ -1,13 +1,54 @@
 package bu.nopachai.butraffic;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Explicit ประกาศตัวแปร
+    private ListView trafficListView;
+    private Button aboutMeButton;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Bind Widget
+        bindwidget();
+
+        //Button Controller
+        buttonController();
+
+    }   // Main Method
+
+    private void buttonController() {
+        aboutMeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //Sound Effect
+                MediaPlayer buttonPlayer = MediaPlayer.create(getBaseContext(), R.raw.dog);
+                buttonPlayer.start();
+
+                //Intent to WebView
+
+
+
+            }   //event
+        });
     }
-}
+
+    private void bindwidget() {
+        trafficListView = (ListView) findViewById(R.id.listView);
+        aboutMeButton = (Button) findViewById(R.id.button);
+    }
+
+}   // Main Class คลาสหลัก
