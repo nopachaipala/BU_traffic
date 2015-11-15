dc6b7a5683d5ced409021771c3b732b88c641ed7
BookBik <nopachai.pala@bumail.net>
2015-11-15 10:45:49 +0700

    Finish day1

--- app/src/main/java/bu/nopachai/butraffic/MainActivity.java
+++ app/src/main/java/bu/nopachai/butraffic/MainActivity.java
@@ -16,8 +16,6 @@ public class MainActivity extends AppCompatActivity {
     private Button aboutMeButton;
 
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,7 +27,48 @@ public class MainActivity extends AppCompatActivity {
         //Button Controller
         buttonController();
 
+        //Create ListView
+        createListview();
+
     }   // Main Method
+
+    private void createListview() {
+
+        //Setup Array
+        String[] strTitle = new String[20];
+        strTitle[0] = "ห้ามเลี้ยวซ้าย";
+        strTitle[1] = "ห้ามเลี้ยวขวา";
+        strTitle[2] = "ให้ตรงไป";
+        strTitle[3] = "ให้เลี้ยวขวา";
+        strTitle[4] = "ให้เลี้ยวซ้าย";
+        strTitle[5] = "ทางออก";
+        strTitle[6] = "ทางเข้า";
+        strTitle[7] = "ทางออก";
+        strTitle[8] = "ให้หยุดรถ";
+        strTitle[9] = "จำกัดความสูง 2.50 เมตร";
+        strTitle[10] = "ทางแยก";
+        strTitle[11] = "ห้ามกลับรถ";
+        strTitle[12] = "ห้ามจอด";
+        strTitle[13] = "ทางรถสวน";
+        strTitle[14] = "ห้ามแซง";
+        strTitle[15] = "ทางเข้า";
+        strTitle[16] = "ให้หยุดตรวจ";
+        strTitle[17] = "จำกัดความเร็ว 50 กิโลเมตร";
+        strTitle[18] = "จำกัดความกว้าง 2.5 เมตร";
+        strTitle[19] = "จำกัดความสูง 5.0 เมตร";
+
+        int[] intIcon = {R.drawable.traffic_01, R.drawable.traffic_02,
+                R.drawable.traffic_03, R.drawable.traffic_04, R.drawable.traffic_05,
+                R.drawable.traffic_06, R.drawable.traffic_07, R.drawable.traffic_08,
+                R.drawable.traffic_09, R.drawable.traffic_10, R.drawable.traffic_11,
+                R.drawable.traffic_12, R.drawable.traffic_13, R.drawable.traffic_14,
+                R.drawable.traffic_15, R.drawable.traffic_16, R.drawable.traffic_17,
+                R.drawable.traffic_18, R.drawable.traffic_19, R.drawable.traffic_20};
+
+        MyAdapter objMyAdapter = new MyAdapter(MainActivity.this, strTitle, intIcon);
+        trafficListView.setAdapter(objMyAdapter);
+
+    }   //Create ListView
 
     private void buttonController() {
         aboutMeButton.setOnClickListener(new View.OnClickListener() {
